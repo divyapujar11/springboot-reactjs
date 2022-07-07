609997119b41b7ecad64b8753db9aa43377a7111
divya pujar <divyapujar0@gmail.com>
2022-07-07 11:24:04 +0530

    created course-app

---
+++ course-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'reactstrap';
+
+function App() {
+  return (
+    <div>
+      <h1> this is button components</h1>
+      <Button>first React button</Button>
+    </div>
+  );
+}
+
+export default App;
