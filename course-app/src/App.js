eab996a785d5f1ef1de6d08d3f028873b29b1fb4
divya pujar <divyapujar0@gmail.com>
2022-07-07 13:35:53 +0530

    update course-app

--- course-app/src/App.js
+++ course-app/src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'reactstrap';
+import {ToastContainer, toast} from 'react-toastify';
 
 function App() {
+  const btnhandle = () =>{
+    toast("my first toast meassage");
+    
+    toast.success("DONE",{
+      position:'top-center'
+    })
+
+    toast.error("SOMETHING IS WRONG",{
+      position:'bottom-left'
+    })
+  }
   return (
     <div>
+      <ToastContainer/>
       <h1> this is button components</h1>
-      <Button>first React button</Button>
+      <Button color='success' outline onClick={btnhandle}>First React Button</Button>
     </div>
   );
 }
